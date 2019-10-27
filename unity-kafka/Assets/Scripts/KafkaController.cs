ffeb5c9a5746c6870726c68ee09fd2265ea0b41a
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-10-27 12:56:50 -0500

    This flow should work but Unity freezes after I click the button

--- unity-kafka/Assets/Scripts/KafkaController.cs
+++ unity-kafka/Assets/Scripts/KafkaController.cs
@@ -13,11 +13,16 @@ namespace unitykafka
 		ConsumerConfig config;
 		bool consuming = false; // initially, start consuming
 		int counter = 0;
+		IConsumer<Ignore, string> c;
+		CancellationTokenSource cts;
 
 		// Start is called before the first frame update
 		void Start()
 		{
 			print("KafkaController start");
+			c = new ConsumerBuilder<Ignore, string>(getConfig()).Build();
+			c.Subscribe("testTopicName");
+			cts = new CancellationTokenSource();
 
 		}
 
@@ -40,47 +45,38 @@ namespace unitykafka
 
 		void consumeFromKafka()
 		{
-			//var conf = new ConsumerConfig
-			//{
-			//	GroupId = "test-consumer-group",
-			//	BootstrapServers = "localhost:9092",
-			//	AutoOffsetReset = AutoOffsetReset.Earliest
-			//};
-			using (var c = new ConsumerBuilder<Ignore, string>(getConfig()).Build())
-			{
-				c.Subscribe("testTopicName");
+			////using (var c = new ConsumerBuilder<Ignore, string>(getConfig()).Build())
+			////{
 
-				CancellationTokenSource cts = new CancellationTokenSource();
-
-				try
-				{
-					print("while (true)");
-					while (true)
-					{
-						try
-						{
-							var cr = c.Consume(cts.Token);
-							print($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
-							GameLogic.msgList.Add(cr.Value);
-						}
-						catch (ConsumeException e)
-						{
-							print("ConsumeException:");
-							print($"Error occured: {e.Error.Reason}");
-						}
-					}
-				}
-				catch (OperationCanceledException e)
-				{
-					print("OperationCanceledException:");
-					print(e.ToString());
-					// Ensure the consumer leaves the group cleanly and final offsets are committed.
-					c.Close();
-				}
-				print("while (true) closed");
-			}
-			print("using() closed");
-			consuming = false; //signal that consuming ended for some reason
+			//	try
+			//	{
+			//		print("while (true)");
+			//		while (true)
+			//		{
+			//			try
+			//			{
+			//				var cr = c.Consume(cts.Token);
+			//				print($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+			//				GameLogic.msgList.Add(cr.Value);
+			//			}
+			//			catch (ConsumeException e)
+			//			{
+			//				print("ConsumeException:");
+			//				print($"Error occured: {e.Error.Reason}");
+			//			}
+			//		}
+			//	}
+			//	catch (OperationCanceledException e)
+			//	{
+			//		print("OperationCanceledException:");
+			//		print(e.ToString());
+			//		// Ensure the consumer leaves the group cleanly and final offsets are committed.
+			//		c.Close();
+			//	}
+			//	print("while (true) closed");
+			////}
+			//print("using() closed");
+			//consuming = false; //signal that consuming ended for some reason
 		}
 
 		public void btn_connect()
@@ -89,38 +85,58 @@ namespace unitykafka
 			consuming = true;
 		}
 
-		void Update1()
-		{
-			print("new msg");
-		}
-
 		void Update()
 		{
-			print(". - .");
 			if (consuming)
 			{
-				//int counter = 0;
-				//while (consuming)
-				//{
-				if (counter > 0) { /*print("counter " + counter);*/ }
-				else { print("counter 0"); }
+				try
+				{
+					var cr = c.Consume(cts.Token);
+					print($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+					GameLogic.msgList.Add(cr.Value);
+				}
+				catch (ConsumeException e)
+				{
+					print("ConsumeException:");
+					print($"Error occured: {e.Error.Reason}");
+				}
+				catch (OperationCanceledException e)
+				{
+					print("OperationCanceledException:");
+					print(e.ToString());
+					// Ensure the consumer leaves the group cleanly and final offsets are committed.
+					c.Close();
+				}
+			}
+}
 
-				counter += 1;
-				GameLogic.msgList.Add("counter" + counter);
-				if (counter > 10000) { print("loop done"); counter = 0; consuming = false; }
-				//consumeFromKafka();
-				//if (consuming == false) // only if loop errors
-				//{
-				//	print("Rec'd false consuming bool, resetting");
-				//	consuming = true;
-				//}
-				//}
-				//print("consuming done");
-			}
-			else
-			{
-				print(".");
-			}
+		void Update2()
+		{
+			//print(". - .");
+			//if (consuming)
+			//{
+			//	//int counter = 0;
+			//	//while (consuming)
+			//	//{
+			//	if (counter > 0) { /*print("counter " + counter);*/ }
+			//	else { print("counter 0"); }
+
+			//	counter += 1;
+			//	GameLogic.msgList.Add("counter" + counter);
+			//	if (counter > 10000) { print("loop done"); counter = 0; consuming = false; }
+			//	//consumeFromKafka();
+			//	//if (consuming == false) // only if loop errors
+			//	//{
+			//	//	print("Rec'd false consuming bool, resetting");
+			//	//	consuming = true;
+			//	//}
+			//	//}
+			//	//print("consuming done");
+			//}
+			//else
+			//{
+			//	print(".");
+			//}
 		}
 
 		void print(string msg)
